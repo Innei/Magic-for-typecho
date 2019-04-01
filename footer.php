f75ac22ad0147646f5ed0f7d1fa7ad18e25acf6a
yiny <yi00oi9hhg@qq.com>
2019-04-01 18:30:33 +0800

    modified:   footer.php 	deleted:    js/rocket-loader.min.js

--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
-<script src="<?php $this->options->themeUrl('/js/dynamics.min.js');?>" type="e8569ed2e22d85714d74224f-text/javascript"></script>
-<script src="<?php $this->options->themeUrl('/js/tinycolor.js');?>" type="e8569ed2e22d85714d74224f-text/javascript"></script>
-<script src="<?php $this->options->themeUrl('/js/mv.es5.js');?>" type="e8569ed2e22d85714d74224f-text/javascript"></script>
-<script src="<?php $this->options->themeUrl('/js/rocket-loader.min.js');?>" data-cf-settings="e8569ed2e22d85714d74224f-|49" defer=""></script>
+<script src="<?php $this->options->themeUrl('/js/dynamics.min.js');?>" type="text/javascript"></script>
+<script src="<?php $this->options->themeUrl('/js/tinycolor.js');?>" type="text/javascript"></script>
+<script src="<?php $this->options->themeUrl('/js/mv.es5.js');?>" type="text/javascript"></script>
 </body>
 </html>
